b04d36be91d484391cdd22378110d979237e6f84
philipiaeveline <philipiaeveline@gmail.com>
2020-10-16 18:32:18 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,17 +1,73 @@
-$(document).ready(function() {
-    $(".eve").click(function() {
-        $(".eve").hide()
-        $(".eve1").show();
-    });
+// $(document).ready(function() {
+//     $(".eve").click(function() {
+//         $(".eve").hide()
+//         $(".eve1").show();
+//     });
     
-    $(".eve1").click(function() {
-        $(".eve1").hide()
-        $(".eve").show();
-    });
+//     $(".eve1").click(function() {
+//         $(".eve1").hide()
+//         $(".eve").show();
+//     });
+// });
+
+//     function eve() {
+//         alert("Hello");
+//     }
+
+
+
+    $(document).ready(function(){
+        
+   
+    $("#img1").hover(function(){
+        $(".card-title1").toggle();
     });
 
-    function eve() {
-        alert("Hello");
-    }
+})
 
-    
+$(document).ready(function(){
+        
+   
+    $("#img2").hover(function(){
+        $(".card-title2").toggle();
+    });
+
+})
+
+$(document).ready(function(){
+        
+   
+    $("#img3").hover(function(){
+        $(".card-title3").toggle();
+    });
+
+})
+$(document).ready(function(){
+        
+   
+    $("#img4").hover(function(){
+        $(".card-title4").toggle();
+    });
+
+})
+
+$(document).ready(function(){
+        
+   
+    $("#img5").hover(function(){
+        $(".card-title5").toggle();
+    });
+
+})
+
+$(document).ready(function(){
+        
+   
+    $("#img6").hover(function(){
+        $(".card-title6").toggle();
+    });
+
+})
+
+
+   
