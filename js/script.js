fb99c2d40aac519fc1f6c1d95fdccbec7a88a489
philipiaeveline <philipiaeveline@gmail.com>
2020-10-16 12:46:19 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".eve").click(function() {
+        $(".eve").hide()
+        $(".eve1").show();
+    });
+    
+    $(".eve1").click(function() {
+        $(".eve1").hide()
+        $(".eve").show();
+    });
+    });
+
+    function eve() {
+        alert("Hello");
+    }
+
+    
